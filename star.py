5137b53eeafe4984fbb4a6600eac39472095e161
nupur-thakur <nupur.s.thakur@gmail.com>
2019-12-22 19:19:34 +0530

    Update star.py

--- star.py
+++ star.py
@@ -1,4 +1,11 @@
 import sys
 #Creates a star pattern with user entered number of rows
 [print(' '*(int(sys.argv[1])-x-1)+'*'*(2*x+1) ) for x in range(int(sys.argv[1]))]
+#I/P : 5
+#O/P :
+      *
+     ***
+    *****
+   *******
+  *********
 
